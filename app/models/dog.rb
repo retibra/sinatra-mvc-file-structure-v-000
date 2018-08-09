7119bfa008e09dadaa313ad0e0fed8f8aae9283d
Timothy Ege <retibra@gmail.com>
2018-08-09 17:56:50 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,11 +2,14 @@ class Dog
 attr_accessor :name, :breed, :age
 
 
-def initialize(name, breed, age)
-  @name = name
-  @breed = breed
-  @age = age
-end
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
+  def self.all
+    @@all
+  end
 
 end
